c9732ed391aab710a2f225cbb50b36fe619da15b
Takamichi Omori <merrytakamerry@gmail.com>
2020-10-28 00:00:58 +0900

    add Gemfile

--- src/main.rb
+++ src/main.rb
@@ -4,7 +4,6 @@ BOT_TOKEN = ENV['DISCORD_BOT_TOKEN']
 BOT_TOKEN.freeze
 bot = Discordrb::Commands::CommandBot.new token: BOT_TOKEN, prefix: '!'
 
-
 bot.command :user do |event|
   # Commands send whatever is returned from the block to the channel. This allows for compact commands like this,
   # but you have to be aware of this so you don't accidentally return something you didn't intend to.

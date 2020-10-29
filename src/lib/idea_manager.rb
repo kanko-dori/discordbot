d61c041841a179db2ebe18140a69d4a496f0bf90
Takamichi Omori <merrytakamerry@gmail.com>
2020-10-29 05:24:49 +0000

    add CRUD

--- src/lib/idea_manager.rb
+++ src/lib/idea_manager.rb
@@ -1,11 +1,51 @@
 # IdeaManager manage ideas
+require 'pg'
 
-class IdeaManager
+module IdeaManager
+  def getConnection
+    username = ENV['POSTGRES_USER']
+    password = ENV['POSTGRES_PASSWORD']
+    host = ENV['POSTGRES_HOST']
+    database = ENV['POSTGRES_DATABASE']
+    port = ENV['POSTGRES_PORT']
+    return PG::connect(:host => host, :user => user, :password => password, :dbname => database)
+  end
+
+  module_function
   def hello(name)
     "Hello, #{name}!"
   end
 
   def addIdea(idea)
-    "add idea: #{idea}"
+    username = ENV['POSTGRES_USER']
+    password = ENV['POSTGRES_PASSWORD']
+    host = ENV['POSTGRES_HOST']
+    database = ENV['POSTGRES_DATABASE']
+    port = ENV['POSTGRES_PORT']
+    conn = PG::connect(:host => host, :user => username, :password => password, :dbname => database)
+
+    sql = "INSERT INTO ideas (idea) VALUES ('#{idea}')"
+    conn.exec(sql)
+    conn.finish
+  end
+
+  def deleteIdea(idea)
+    "delete idea: #{idea}"
+  end
+
+  def listIdea
+    username = ENV['POSTGRES_USER']
+    password = ENV['POSTGRES_PASSWORD']
+    host = ENV['POSTGRES_HOST']
+    database = ENV['POSTGRES_DATABASE']
+    port = ENV['POSTGRES_PORT']
+    conn = PG::connect(:host => host, :user => username, :password => password, :dbname => database)
+
+    sql = "SELECT * FROM ideas"
+    res = conn.exec(sql)
+    res.each do |r|
+      p r
+    end
+    res
   end
 end
--- src/main.rb
+++ src/main.rb
@@ -9,13 +9,33 @@ bot.command :user do |event|
   # Commands send whatever is returned from the block to the channel. This allows for compact commands like this,
   # but you have to be aware of this so you don't accidentally return something you didn't intend to.
   # To prevent the return value to be sent to the channel, you can just return `nil`.
-  mgr = IdeaManager.new
-  mgr.hello(event.user.name)
+  username = event.user.name
+
+  puts ":user involed: #{username}"
+  IdeaManager.hello(username)
 end
 
 bot.command :add do |event|
-  mgr = IdeaManager.new
-  mgr.addIdea(event.message.content.split[1])
+  content = event.message.content.gsub(/!add /, '')
+
+  puts ":add invoked: #{content}"
+  IdeaManager.addIdea(content)
+end
+
+bot.command :delete do |event|
+  content = event.message.content.gsub(/!delete /, '')
+  
+  puts ":delete invoked: #{content}"
+  IdeaManager.deleteIdea(content)
+end
+
+bot.command :list do |event|
+  puts ':list invoked'
+
+  ideas = IdeaManager.listIdea
+  ideas.map do |idea|
+    "id: #{idea['id']}, idea: #{idea['idea']}, detail: #{idea['detail']}"
+  end
 end
 
 bot.run
